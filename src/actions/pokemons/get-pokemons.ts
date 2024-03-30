cd12cef5132d5e7830c3dfd69613279e5ca32298
javierestrada26 <javiestrada14@hotmail.com>
2024-03-29 23:03:43 -0500

    Scroll infinito, info personajes,

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -12,7 +12,7 @@ export const getPokemons = async (page:number, limit:number=20):Promise<Pokemon[
         const url = `/pokemon?offset=${page *10 }&limit=${limit}`
         const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url)
 
-        const pokemonPromises = data.results.map( (info) => {
+        const pokemonPromises = data.results.map( info => {
             return pokeApi.get<PokeAPIPokemon>(info.url)
         })
 
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { FlatList, StyleSheet, View } from 'react-native'
 import {  Text } from 'react-native-paper'
 import { getPokemons } from '../../../actions/pokemons/get-pokemons'
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery,  useQueryClient } from '@tanstack/react-query';
 import { PokeballBg } from '../../components/ui/PokeballBg'
 import { globalTheme } from '../../../config/theme/global-theme'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
@@ -11,12 +11,29 @@ import { PokemonCard } from '../../components/pokemons/PokemonCard'
 export const HomeScreen = () => {
 
   const {top} = useSafeAreaInsets()
+  const queryClient = useQueryClient()
 
-  const {isLoading, data : pokemons  = []} = useQuery({
+  /** esta es la forma tradiconal de una forma http
+   * 
+   * const {isLoading, data : pokemons  = []} = useQuery({
     queryKey: ['pokemons'],
     queryFn: ()=>getPokemons(0),
     staleTime: 1000 * 60 * 60 // 1 hour
-  
+  })
+   */
+
+  const {isLoading, data,fetchNextPage } = useInfiniteQuery({
+    queryKey: ['pokemons','infinite'],
+    initialPageParam: 0,
+    staleTime: 1000 * 60 * 60, // 1 hour 
+    queryFn: async (params)=>{
+      const pokemons = await getPokemons(params.pageParam)
+      pokemons.forEach(pokemon => {
+        queryClient.setQueryData(['pokemon',pokemon.id],pokemon)
+      } )
+      return pokemons
+    },
+    getNextPageParam: (lastPage, pages) => pages.length, 
   })
 
 
@@ -24,7 +41,7 @@ export const HomeScreen = () => {
     <View style={globalTheme.globalMargin}>
       <PokeballBg style={styles.imgPosition}/>
       <FlatList
-        data={pokemons}
+        data={data?.pages.flat()?? []}
         keyExtractor={(pokemon,index) => `${pokemon.id}-${index}`}
         numColumns={2}
         style = {{paddingTop:top + 20}}
@@ -32,6 +49,9 @@ export const HomeScreen = () => {
           <Text variant='displayMedium'>Pokedex</Text>
         )}
         renderItem={({item}) => <PokemonCard pokemon={item}/>} 
+        onEndReachedThreshold={0.6}
+        onEndReached={()=>fetchNextPage()}
+        showsVerticalScrollIndicator={false}
       />
     </View>
   )
